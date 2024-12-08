708a82ceb16ca9ef8afb46a07307ad31f1791f96
Guilherme Campelo <campelo.guilherme@hotmail.com>
2024-12-07 21:41:31 -0300

    Adicionar arquivos de projeto.

---
+++ WhileC#/Exercicio1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileC_
+{
+    internal class Exercicio1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Contadora do 1 ao 10\n");
+            int contadora = 1; 
+            while (contadora <= 10)
+            {
+                Console.WriteLine("Contando : " + contadora);
+                contadora++;
+            }
+        }
+    }
+}
---
+++ WhileC#/Program.cs
@@ -0,0 +1,8 @@
+﻿using WhileC_;
+
+//Exercicio1.Main(args);
+//Exercicio2.Main(args);
+//Exercicio3.Main(args);
+//Exercicio4.Main(args);
+//Exercicio5.Main(args);
+Exercicio6.Main(args);
